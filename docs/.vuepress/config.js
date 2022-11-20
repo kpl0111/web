8654e698c56571beebc356186ec0e05849e8844b
kpl0111 <1911438@mail.nankai.edu.cn>
2022-11-20 14:09:22 +0800

    change root

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    base: '/vuepress-demo/',
     title: 'Nefelibata',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
       ['link', { rel: 'icon', href: '/avatar.png' }], // 增加一个自定义的 favicon(网页标签的图标)
