ea98edf9e72dfd460c4c0bd37cd436c53c06e85c
kpl0111 <1911438@mail.nankai.edu.cn>
2022-11-20 13:29:02 +0800

    source file

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:nan-gong/nan-gong.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:kpl0111/web.git master:main
+git push -f git@github.com:kpl0111/web.git master:master
 
 cd -
